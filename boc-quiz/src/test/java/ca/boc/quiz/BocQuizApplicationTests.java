8b504ffd48f4693adfed964da5984b8ce577820d
geoff <geoff@Geoffs-Laptop.dioscuri.ca>
2020-09-23 11:56:59 -0400

    Initial commit of project

---
+++ boc-quiz/src/test/java/ca/boc/quiz/BocQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.boc.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BocQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
