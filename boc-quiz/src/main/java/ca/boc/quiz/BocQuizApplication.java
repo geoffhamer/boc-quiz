7efce17a9d926e89c0949ac24781fda1b4717923
geoff <geoff@Geoffs-Laptop.dioscuri.ca>
2020-09-24 14:24:55 -0400

    Added check that csv file location is provided

--- boc-quiz/src/main/java/ca/boc/quiz/BocQuizApplication.java
+++ boc-quiz/src/main/java/ca/boc/quiz/BocQuizApplication.java
@@ -1,5 +1,7 @@
 package ca.boc.quiz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,6 +11,8 @@ import ca.boc.quiz.service.ClimateDataParsingServiceImpl;
 @SpringBootApplication
 public class BocQuizApplication {
 
+	private final static Logger LOG = LoggerFactory.getLogger(BocQuizApplication.class);
+	
 	public static void main(String[] args) {
 		
 		String fileLocation = "C:/temp/eng-climate-summary.csv";
@@ -19,11 +23,20 @@ public class BocQuizApplication {
 		     args[0] != null ) {
 			
 			fileLocation = args[0];
+
+			ClimateDataParsingServiceImpl service = (ClimateDataParsingServiceImpl)context.getBean(ClimateDataParsingServiceImpl.class);
+			service.initClimateData(fileLocation);
+		
+		} else {
+			
+			String errStr = "The location of a .csv file must be provided to execute this application.";
+			
+			LOG.error(errStr);
+			System.out.println(errStr);
+			System.out.println("Example: java -jar boc-quiz.jar <csv file location>");
+			
 		}
 		
-		// todo: Replace with startup listener
-		ClimateDataParsingServiceImpl service = (ClimateDataParsingServiceImpl)context.getBean(ClimateDataParsingServiceImpl.class);
-		service.initClimateData(fileLocation);
 	}
 
 }
