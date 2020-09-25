96b6dd0d9fbb855274dea4e2ade711520c9932ca
geoff <geoff@Geoffs-Laptop.dioscuri.ca>
2020-09-25 07:56:24 -0400

    Added custom exception

---
+++ boc-quiz/src/main/java/ca/boc/quiz/exception/DataParsingException.java
@@ -0,0 +1,11 @@
+package ca.boc.quiz.exception;
+
+public class DataParsingException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public DataParsingException(String message) {
+		super(message);
+	}
+
+}
